64ecc89bf48136115d1da590319dfad569f4e974
Dkazem91 <Dkazemian@gmail.com>
2018-06-07 18:41:26 +0000

    added changed updated_at to another now call to match example output

--- models/base_model.py
+++ models/base_model.py
@@ -17,7 +17,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
     def __str__(self):
         return "[{}] ({}) {}".format(
