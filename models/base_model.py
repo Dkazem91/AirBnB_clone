077c3c12576b1cdf3fdac591b8c63ef7f1301907
Dkazem91 <Dkazemian@gmail.com>
2018-06-07 16:00:21 +0000

    pep8ed

--- models/base_model.py
+++ models/base_model.py
@@ -3,6 +3,7 @@
 import uuid
 from datetime import datetime
 
+
 class BaseModel:
     def __init__(self):
         self.id = str(uuid.uuid4())
@@ -10,7 +11,8 @@ class BaseModel:
         self.updated_at = self.created_at
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return "[{}] ({}) {}".format(
+            self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
         self.updated_at = datetime.now()
