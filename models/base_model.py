a2394c64a8f3f88c56ddd9e44e36433cc0f827dc
Andrew S <andrew.suh.94@gmail.com>
2018-06-07 16:04:25 -0700

    Changed  type uuid4 to str and returned the dictionary in function

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""BaseModel that defines all common attributes/methods for other classes:"""
 from uuid import uuid4
 from datetime import datetime
-"""BaseModel that defines all common attributes/methods for other classes:"""
+
 
 class BaseModel:
         """
@@ -9,7 +10,7 @@ class BaseModel:
         """
 
         def __init__(self):
-                self.id = uuid4()
+                self.id = str(uuid4())
                 self.created_at = datetime.now()
                 self.updated_at = self.created_at
 
@@ -23,3 +24,4 @@ class BaseModel:
                 self.__dict__["__class__"] = self.__class__.__name__
                 self.__dict__["created_at"] = str(self.created_at.isoformat())
                 self.__dict__["updated_at"] = str(self.updated_at.isoformat())
+                return self.__dict__
