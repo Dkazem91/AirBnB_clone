763a6098f3ad00d61bedf8868c847273cc7abc3e
Dkazem91 <Dkazemian@gmail.com>
2018-06-07 18:34:13 +0000

    added kwargs to init

--- models/base_model.py
+++ models/base_model.py
@@ -5,10 +5,19 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        keys = {'id', 'created_at', 'updated_at'}
+        if kwargs:
+            for key, value in kwargs.items():
+                if key in keys:
+                    if key == 'created_at' or key == 'updated_at':
+                        value = datetime.strptime(
+                            value, "%Y-%m-%dT%H:%M:%S.%f")
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         return "[{}] ({}) {}".format(
