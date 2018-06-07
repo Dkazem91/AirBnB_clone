a968e138202afdb78d775b7780bf9f9efe4d6541
Andrew S <andrew.suh.94@gmail.com>
2018-06-07 15:58:39 -0700

    Added to __str__ function

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
                 self.updated_at = self.created_at
 
         def __str__(self):
-                
+                return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
         def save(self):
                 self.updated_at = datetime.now()
