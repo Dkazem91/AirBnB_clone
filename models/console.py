3805a7b6a3e72fd8d092c8bad276659390dacf47
Dkazem91 <Dkazemian@gmail.com>
2018-06-08 15:19:46 +0000

    created prompt, added quit and EOF commands

--- models/console.py
+++ models/console.py
@@ -1,2 +1,16 @@
-#!/user/bin/python3
+#!/usr/bin/python3
 """entry point of the command interpreter"""
+import cmd
+import models
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb)'
+
+    def do_quit(self, line):
+        "Quit command to exit the program"
+        return True
+
+    do_EOF = do_quit
+
+if __name__=='__main__':
+    HBNBCommand().cmdloop()
