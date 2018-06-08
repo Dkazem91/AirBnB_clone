b39907162b582d3c5b22f602b94d992d72743219
Dkazem91 <Dkazemian@gmail.com>
2018-06-08 15:40:17 +0000

    moved console to airbnb

--- console.py
+++ console.py
@@ -3,6 +3,7 @@
 import cmd
 import models
 
+
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb)'
 
@@ -11,6 +12,12 @@ class HBNBCommand(cmd.Cmd):
         return True
 
     do_EOF = do_quit
+
+    def do_create(self, line):
+        if not len(line):
+            print("** class name missing **")
+        for x in line:
+            print(x)
 
 if __name__=='__main__':
     HBNBCommand().cmdloop()
