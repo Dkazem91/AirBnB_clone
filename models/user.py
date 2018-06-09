291b8e1a707352e764c0f2de351424ed5499e770
Dkazem91 <Dkazemian@gmail.com>
2018-06-09 14:16:45 +0000

    populated user class

--- models/user.py
+++ models/user.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 """class User that inherits from BaseModel"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
