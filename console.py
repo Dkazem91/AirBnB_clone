aefaa644a47505b0b37485e95834d7a1dc5b58a6
Andrew S <andrew.suh.94@gmail.com>
2018-06-08 20:35:15 -0700

    Added destroy functionality

--- console.py
+++ console.py
@@ -7,7 +7,6 @@ from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
         prompt = "(hbnb) "
-        list_keys = {"BaseModel", "Amenity", "City", "State", "Place", "Review"}
 
         def do_quit(self, arg):
                 """Quit command to exit the program"""
@@ -20,7 +19,7 @@ class HBNBCommand(cmd.Cmd):
                 if len(arg) == 0:
                         print("** class name missing **")
                         return
-                if arg not in HBNBCommand.list_keys:
+                if arg not in dir(models.base_model):
                         print("** class doesn't exist **")
                         return
                 else:
@@ -48,10 +47,27 @@ class HBNBCommand(cmd.Cmd):
                         print("** no instance found **")
 
         def do_destroy(self, arg):
-                pass
-                
-                
+                if len(arg) == 0:
+                        print("** class name missing **")
+                        return
+                strings = arg.split()
+                if strings[0] not in dir(models.base_model):
+                        print("** class doesn't exist **")
+                        return
+                if len(strings) < 2:
+                        print("** instance id missing **")
+                        return
+                try:
+                        del(storage.all()[strings[0] + "." + strings[1]])
+                        storage.save()
+                except:
+                        print("** no instance found **")
 
+        def do_all(self, arg):
+                strings = arg.split()
+                if strings[0] not in dir(models.base_model):
+                        print("** class doesn't exist **")
+                        return
 
 
 if __name__ == '__main__':
