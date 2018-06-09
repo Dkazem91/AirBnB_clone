fbe0acfafd98742e215e1634fd50ce870e2c6cb2
Andrew S <andrew.suh.94@gmail.com>
2018-06-08 21:29:33 -0700

    Added repr for string output

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,12 @@ class BaseModel:
                 """
                 return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
+        def __repr__(self):
+                """
+                returns __str__
+                """
+                return self.__str__()
+
         def save(self):
                 """
                 changes updated_at time
