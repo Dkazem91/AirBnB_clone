5f15919be5037f924dde41ca3cc020fd51c82993
Andrew S <andrew.suh.94@gmail.com>
2018-06-08 18:50:46 -0700

    Created a list of classes to compare to and back up changes

--- console.py
+++ console.py
@@ -1,10 +1,12 @@
 #!/user/bin/python3
 """entry point of the command interpreter"""
 import cmd
+from models import storage
 from models.base_model import BaseModel
 
 class HBNBCommand(cmd.Cmd):
         prompt = "(hbnb) "
+        list_keys = {"BaseModel", "Amenity", "City", "State", "Place", "Review"}
 
         def do_quit(self, arg):
                 """Quit command to exit the program"""
@@ -14,7 +16,7 @@ class HBNBCommand(cmd.Cmd):
 
         def do_create(self, arg):
                 """Create an instance"""
-                if len(arg) < 1:
+                if arg not in HBNBCommand.list_keys:
                         print("** class name missing **")
                 else:
                         try:
@@ -23,7 +25,18 @@ class HBNBCommand(cmd.Cmd):
                                 print(new.id)
                         except:
                                 print("** class doesn't exist **")
-        def do_show
+        def do_show(self, arg):
+                if not len(line):
+                        print("** class name missing **")
+                        return
+                strings = arg.split()
+                if strings[0] not in HBNBCommand.list_keys:
+                        print("** class name missing **")
+                        return
+                
+                
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
