07eb26aa2b78ccde27725bab37796bbdeb702933
Dkazem91 <Dkazemian@gmail.com>
2018-06-09 14:15:59 +0000

    updated file_storage importsfor all classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -3,23 +3,31 @@
 import os.path
 import json
 from models.base_model import BaseModel
-
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.Place import Place
+from models.Review import Review
 
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
+        """returns the dictionary of objects"""
         return FileStorage.__objects
 
     def new(self, obj):
+        """adds new object to __objects"""
         name = obj.__class__.__name__
         FileStorage.__objects[name + '.' + obj.id] = obj
 
     def save(self):
-            jsonData = {}
-            for key, value in FileStorage.__objects.items():
-                jsonData[key] = value.to_dict()
+        """saves objects to json file"""
+        jsonData = {}
+        for key, value in FileStorage.__objects.items():
+            jsonData[key] = value.to_dict()
             with open(FileStorage.__file_path, 'w') as f:
                 data = json.dump(jsonData, f)
 
