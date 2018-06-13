d05fa041d9b40f9a5042e565f118e62f0096dcd9
Andrew S <Donnerschlag@users.noreply.github.com>
2018-06-12 22:58:30 -0700

    changed functions

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -17,7 +17,9 @@ class TestBaseModel(unittest.TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        """tears down"""
+        """
+        tears down
+        """
         del cls.testBase
         try:
             os.remove("file.json")
@@ -25,32 +27,33 @@ class TestBaseModel(unittest.TestCase):
             pass
 
     def test_pep8_basemodel(self):
-        """tests pep8"""
+        """
+        tests pep8
+        """
         style = pep8.StyleGuide(quiet=True)
         p = style.check_files(['models/base_model.py'])
         self.assertEqual(p.total_errors, 0, "fix pep8")
 
-    def test_docstrings_basemodel(self):
-        self.assertTrue(len(BaseModel.__doc__) > 0)
-        for func in dir(BaseModel):
-            self.assertTrue(len(func.__doc__) > 0)
+    def test_check_functions(self):
+        self.assertIsNotNone(BaseModel.__doc__)
+        self.assertIsNotNone(BaseModel.save.__doc__)
+        self.assertIsNotNone(BaseModel.to_dict.__doc__)
 
-    def test_functions_basemodel(self):
+    def test_attribute_basemodel(self):
         self.assertTrue(hasattr(BaseModel, "__init__"))
         self.assertTrue(hasattr(BaseModel, "save"))
         self.assertTrue(hasattr(BaseModel, "to_dict"))
-        self.assertTrue(self.testBase.created_at == self.testBase.updated_at)
 
-    def test_init_basemodel(self):
+    def test_init(self):
         self.assertTrue(isinstance(self.testBase, BaseModel))
 
-    def test_save_basemodel(self):
+    def test_save(self):
         self.testBase.save()
         self.assertNotEqual(self.testBase.created_at, self.testBase.updated_at)
 
-    def test_to_dict_basemodel(self):
+    def test_to_dict(self):
         copy = self.testBase.to_dict()
-        self.assertEqual(self.testBase.__class__.__name__, "BaseModel")
+        self.assertEqual(self.testBase.__class__.__name__, 'BaseModel')
         self.assertIsInstance(copy['created_at'], str)
         self.assertIsInstance(copy['updated_at'], str)
 
